c8f8ccbdb20c41e95cb8862f688cd2bd26ef3931
ljpu <leopu2nd@gmail.com>
2020-04-14 16:48:25 -0400

    Initial copy
    which employs Spring Boot

---
+++ LearnJava8/src/main/java/com/lpu/Developer.java
@@ -0,0 +1,61 @@
+package com.lpu;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Developer {
+
+    String name;
+    BigDecimal salary;
+    LocalDate start;
+    int age;
+
+    public Developer(String name) {
+        this.name = name;
+    }
+
+    public Developer(String name, BigDecimal salary, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDate start) {
+        this.start = start;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+
+}
